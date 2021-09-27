5e6b4da809533ce6b1a57645b4a1faae2a29c90b
Zeki <iziquizel@gmail.com>
2021-09-27 03:00:33 -0300

    Added user registration to API

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Validator = require("validatorjs");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { User, UserRules } = require("../../models/User");
 
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     let valid = new Validator(req.body, UserRules);
     if (!valid.passes()) {
         console.error(valid.errors);
@@ -14,7 +16,46 @@ router.post("/", (req, res) => {
         return;
     }
 
-    res.send("User passed validation");
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                errors: {
+                    email: ["This user is already registered"],
+                },
+            });
+        }
+
+        const avatar = gravatar.url(email, {
+            s: "200",
+            r: "pg",
+            d: "mm",
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password,
+        });
+
+        // @TODO: Encrypt password w/ bcrpyt
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(user.password, salt);
+
+        await user.save();
+
+        // @TODO: Return a JWT
+
+        res.send("User registered");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
