b763139d347292acf673cc16e3bd14b18a5ea135
Zeki <iziquizel@gmail.com>
2021-09-28 03:16:51 -0300

    Full profile creation and update for self (#3)
    * Added full user signin logic; added authorization middleware using JWT; added user sign in API route; created user profile schema; created GET api/profile/me API route
    
    * Fully implemented profile creation and update for self

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,72 @@
 const mongoose = require("mongoose");
 
+const ProfileRegisterRules = {
+    company: "string",
+    website: "string",
+    location: "string",
+    status: "required|string",
+    skills: "string|required",
+    bio: "string",
+    githubusername: "string",
+
+    "experience.*.title": "required|string",
+    "experience.*.company": "required|string",
+    "experience.*.location": "string",
+    "experience.*.from": "required|date",
+    "experience.*.to": "date",
+    "experience.*.current": "boolean",
+    "experience.*.description": "string",
+
+    "education.*.school": "required|string",
+    "education.*.degree": "required|string",
+    "education.*.field": "required|string",
+    "education.*.from": "required|date",
+    "education.*.to": "date",
+    "education.*.current": "boolean",
+    "education.*.description": "string",
+    social: {
+        youtube: "url",
+        twitter: "url",
+        facebook: "url",
+        linkedin: "url",
+        instagram: "url",
+    },
+    date: "date",
+};
+
+const ProfileUpdateRules = {
+    company: "string",
+    website: "string",
+    location: "string",
+    status: "string",
+    skills: "string",
+    bio: "string",
+    githubusername: "string",
+
+    "experience.*.title": "string",
+    "experience.*.company": "string",
+    "experience.*.location": "string",
+    "experience.*.from": "date",
+    "experience.*.to": "date",
+    "experience.*.current": "boolean",
+    "experience.*.description": "string",
+
+    "education.*.school": "string",
+    "education.*.degree": "string",
+    "education.*.field": "string",
+    "education.*.from": "date",
+    "education.*.to": "date",
+    "education.*.current": "boolean",
+    "education.*.description": "string",
+    social: {
+        youtube: "url",
+        twitter: "url",
+        facebook: "url",
+        linkedin: "url",
+        instagram: "url",
+    },
+};
+
 const Profile = mongoose.model(
     "profile",
     new mongoose.Schema({
@@ -103,4 +170,4 @@ const Profile = mongoose.model(
     })
 );
 
-module.exports = { Profile };
+module.exports = { Profile, ProfileRegisterRules, ProfileUpdateRules };
