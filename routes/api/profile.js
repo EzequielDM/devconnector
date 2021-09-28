99b8d75b3c7e2bf23a21c0c77c2d535ec2937b14
Zeki <iziquizel@gmail.com>
2021-09-28 01:46:47 -0300

    Added full user signin logic; added authorization middleware using JWT; added user sign in API route; created user profile schema; created GET api/profile/me API route (#2)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public
-router.get("/", (req, res) => {
-    res.send("Profile route");
+const { User } = require("../../models/User");
+const { Profile } = require("../../models/Profile");
+
+// @route    GET api/profile/me
+// @desc     Return user's profile
+// @access   Private
+router.get("/me", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            "user",
+            ["name", "avatar"]
+        );
+
+        if (!profile) {
+            return res
+                .status(404)
+                .json({ errors: [{ message: "User not found" }] });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
