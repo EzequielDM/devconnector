3b74a34a271a20d5cfea097aa2d873cf838e85f5
EzequielDM <iziquizel@gmail.com>
2022-01-14 22:06:41 -0300

    Added not found page route

--- client/src/components/layout/NotFound.tsx
+++ client/src/components/layout/NotFound.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <>
       <h1 className="x-large text-primary">Oops, where are we?</h1>
-      <p className="large">
+      <p className="lead">
         I'm sure there was supposed to be something here, did I forget a semicolon somewhere?
       </p>
       <small>404 - Page not found (just like zeki's brain)</small>
